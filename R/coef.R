f8938b22c5c5dae5762a0b9785e4e0f3854dc034
alex hayes <alexpghayes@gmail.com>
2018-09-21 20:23:03 -0500

    More notes on specification-object dispatch

--- R/coef.R
+++ R/coef.R
@@ -17,6 +17,23 @@ coef.estimated_fit <- function(estimated_fit) {
 }
 
 ## why is this descent tree useful?
+# why would you not want to just jump immediately to
+# coef(estimated_fit$fit_object)
+#
+# the answer: if you jump straight to predict(estimated_fit$fit_object)
+# all of a sudden you have to deal with predictions from 8 types of models
+# all at once
+#
+# on the prediction side of things, safe_predict will deal with this so it's
+# a moot point
+#
+# what about on the model interpretation side of things, or the model
+# reporting side? think glmnet when you want to create a LaTeX report
+# describing the loss function being minimized but now you have to search
+# through by family and see where everything actually is
+#
+# the idea is to provide some additional guarantees about what's actually
+# been fit, so you know it's reasonable to provide a method for that estimator
 
 coef.glim <- function(model_group, estimated_fit) {
   coef_glim(estimated_fit$specification$model)
